ae5252420e75cf885bd8993781eb2e94c5c989c2
joysreb <53601485+joysreb@users.noreply.github.com>
2019-08-03 20:49:52 +0530

    Create days between dates.py

---
+++ days between dates.py
@@ -0,0 +1,10 @@
+from datetime import date
+start=date(2019,2,2)
+end=date(2019,2,11)
+days=(end-start).days
+
+
+print(f"start date:{start}")
+print(f"end date:{end}")
+print(f"days between days:{days}")
+
