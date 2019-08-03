8a7d2a72c85385443471211f3ed882270eb23534
joysreb <53601485+joysreb@users.noreply.github.com>
2019-08-03 20:48:15 +0530

    Delete days between twodays.py

--- days between twodays.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-start=date(2019,2,2)
-end=date(2019,2,11)
-days=(end-start).days
-
-
-print("start date:{start}")
-print("end date:{end}")
-print("days between days:{days}")
