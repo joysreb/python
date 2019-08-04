0aa5ec256575b5ca8efe83f3b8be78c29ab205f1
joysreb <53601485+joysreb@users.noreply.github.com>
2019-08-04 19:25:14 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,7 @@
+letter=input("enter the alphabet:")
+vowels={'a','e','i','o','u'}
+if letter in vowels:
+             print("it is a vowel")
+else:
+             print("it is not a vowel")
+             
